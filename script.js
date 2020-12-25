d43e200aa75bce372dec1413e0e7baac99af4b95
Aditya Saxena <asaxena1205@gmail.com>
2020-12-25 12:13:50 +0530

    added the UI component with close button and overlay close functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+'use script';
+
+let btnsModal = document.querySelectorAll('.show-modal');
+let modal = document.querySelector('.modal');
+let btnClose = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+
+//Adding open modal functionality to all show modal buttons
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsModal.length; i++) {
+  btnsModal[i].addEventListener('click', openModal);
+}
+
+//Adding close functionality to close button
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClose.addEventListener('click', closeModal);
+
+//Adding close functionality to overlay area
+overlay.addEventListener('click', closeModal);
