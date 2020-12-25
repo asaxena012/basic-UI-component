e08eb355bbdec1d064c9993846fe305851b08c0b
Aditya Saxena <asaxena1205@gmail.com>
2020-12-25 12:33:29 +0530

    added 'Escape' keypress close modal functionality

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ btnClose.addEventListener('click', closeModal);
 
 //Adding close functionality to overlay area
 overlay.addEventListener('click', closeModal);
+
+//Adding Escape keypress close modal functionality
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
